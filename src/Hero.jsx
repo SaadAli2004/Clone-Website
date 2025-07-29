14f71e8e377d0918947229ff27e199dfd4705370
SaadAli2004 <mohammadsaadali16@gmail.com>
2025-07-29 18:25:29 +0500

    Test image

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -4,7 +4,7 @@ function Hero() {
   return (
     <div class="flex p-5 bg-gray-900 gap-5">
       <div>
-        <img src="./src/assets/deal1.jpg" alt="" />
+        <img src="/src/assets/deal1.jpg" alt="" />
       </div>
       <div class="flex flex-col gap-5">
         <img src="./src/assets/deal2.jpg" alt="" />
